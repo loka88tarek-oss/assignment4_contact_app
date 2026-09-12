e7b9b79a9e179ed95a1a46a8544c5784301c475b
Malak <loka88tarek@gmail.com>
2026-09-13 00:41:55 +0300

    modal bottom sheet (not complete)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,7 +13,7 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         leadingWidth: 117,
         leading: Padding(
-          padding: const EdgeInsets.only(left: 10,top: 5, bottom: 5),
+          padding: const EdgeInsets.only(left: 10, top: 5, bottom: 5),
           child: Image.asset(AppAssets.appBarImage),
         ),
       ),
@@ -32,10 +32,86 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      floatingActionButton:FloatingActionButton(onPressed: (){},
-      backgroundColor: AppColors.gold,
-      child: Icon(Icons.add,color: AppColors.darkBlue,size: 30,),
-      )
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            backgroundColor: AppColors.darkBlue,
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: size.height * 0.5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(height: size.height * 0.02),
+                      Row(
+                        children: [
+                          Container(
+                            height: size.height * 0.13,
+                            width: size.width * 0.3,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(color: AppColors.gold),
+                            ),
+                            child: Lottie.asset(
+                              "assets/jsons/image_picker.json",
+                            ),
+                          ),
+                          SizedBox(width: size.width * 0.02),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "User Name",
+                                style: TextStyle(
+                                  color: AppColors.gold,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Divider(color: AppColors.gold, thickness: 5),
+                              Text(
+                                "example@email.com",
+                                style: TextStyle(
+                                  color: AppColors.gold,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Divider(color: AppColors.gold, thickness: 1),
+                              Text(
+                                "+200000000000",
+                                style: TextStyle(
+                                  color: AppColors.gold,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Divider(color: AppColors.gold, thickness: 1),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+
+        backgroundColor: AppColors.gold,
+        child: Icon(Icons.add, color: AppColors.darkBlue, size: 30),
+      ),
     );
   }
 }
