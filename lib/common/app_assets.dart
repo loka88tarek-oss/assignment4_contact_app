355fab35af15806347751a025d62870133ace2ec
Malak <loka88tarek@gmail.com>
2026-09-12 23:20:42 +0300

    empty home screen

---
+++ lib/common/app_assets.dart
@@ -0,0 +1,3 @@
+class AppAssets {
+  static const String appBarImage = 'assets/images/app_bar_image.png';
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,41 @@
+import 'package:assignment4_contact_app/common/app_assets.dart';
 import 'package:assignment4_contact_app/common/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-    
-      
-      
+      appBar: AppBar(
+        leadingWidth: 117,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10,top: 5, bottom: 5),
+          child: Image.asset(AppAssets.appBarImage),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: size.height * 0.09),
+          Lottie.asset("assets/jsons/empty_list.json"),
+          SizedBox(height: size.height * 0.05),
+          Text(
+            "There is No Contacts Added Here",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: AppColors.gold,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton:FloatingActionButton(onPressed: (){},
+      backgroundColor: AppColors.gold,
+      child: Icon(Icons.add,color: AppColors.darkBlue,size: 30,),
+      )
     );
   }
 }
